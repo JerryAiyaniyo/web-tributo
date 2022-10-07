06531884b644408b8a0b49c9bb885e622e04983e
Guillem <liammartinez@Guillems-Macbook-Pro.local>
2022-10-07 09:46:01 +0200

    guillem commit

---
+++ src/components/functions.js
@@ -0,0 +1,5 @@
+let navbarLogo = document.querySelector("#navbarLogo");
+
+navbarLogo.addEventListener('click', () => {
+  document.body.style.backgroundImage = "url('../assets/Images/mask/angus-face-2.png')";
+})
